40d44470dadea0ffee7c88fc7a57152ba911a2f0
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-01 15:21:35 -0400

    hi

--- agents_simple.py
+++ agents_simple.py
@@ -14,15 +14,16 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 llm_model = "gpt-3.5-turbo"
 
 llm = ChatOpenAI(temperature=0.0)
-llm_math = LLMMathChain.from_llm(llm=llm)
 
-math_tool = Tool(
-    name="Calculator",
-    func=llm_math.invoke,
-    description="Useful for when you need to answer questions related to math"
-)
+
+# math_tool = Tool(
+#     name="Calculator",
+#     func=llm_math.invoke,
+#     description="Useful for when you need to answer questions related to math"
+# )
 tools = load_tools(
-    ['llm-math']
+    ['llm-math'], 
+    llm=llm
 )
 
 print(tools[0].name, tools[0].description)
@@ -37,8 +38,10 @@ agent = initialize_agent(
     max_iterations=3 
 )
 
-response = agent.invoke("what is 3.1^2.1")
-print(response)
+query = "what is 3.1^2.1"
+result  = agent.invoke(query)
+print(result["output"])
+
 
 # response = llm.invoke("what is Langchain and how does it work?")
 # print(response.content)
